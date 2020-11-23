2a58d9ced005ad2f658babb46a955f5416f43bad
Jack Hubert <jphubert93@gmail.com>
2020-11-23 21:57:21 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(array_1, array_2)
+  array_1.concat(array_2)
+end
+
+def using_insert(array, element)
+  array.insert(element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten(array)
+end
+
+def using_delete(array, string)
+  arrat.delete(string)
+end
+
+def using_delete_at(array, index)
+  array.delete_at(index)
+end
